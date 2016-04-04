f7f84ca1ae463fd9b8fd2dc96fc51db1e73e723b
PhillMue <muenzuve@gmail.com>
2016-04-04 15:43:41 +0300

    added the submit button method

--- app/src/main/java/mue/com/webapitutorial/MainActivity.java
+++ app/src/main/java/mue/com/webapitutorial/MainActivity.java
@@ -1,7 +1,9 @@
 package mue.com.webapitutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    // This is the method that is called when the submit button is clicked
+
+    public void verifyEmail(View view) {
+
+        EditText emailEditText = (EditText) findViewById(R.id.email_address);
+        String email = emailEditText.getText().toString();
+
+        // TODO, create the task to call the REST API
+
     }
 }
