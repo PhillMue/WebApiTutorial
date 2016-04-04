95b2261a719508bbe0a485d690a5ef1d5c99f944
PhillMue <muenzuve@gmail.com>
2016-04-04 17:13:33 +0300

    added logic to validate email

--- app/src/main/java/mue/com/webapitutorial/MainActivity.java
+++ app/src/main/java/mue/com/webapitutorial/MainActivity.java
@@ -1,25 +1,31 @@
 package mue.com.webapitutorial;
 
+import android.content.Intent;
 import android.os.AsyncTask;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+    public final static String strikeIronUserName = "muenzuve@gmail.com";
+    public final static String strikeIronPassword = "DDvEEo";
+    public final static String apiURL = "http://ws.strikeiron.com/StrikeIron/EMV6Hygiene/VerifyEmail?";
 
-    // This is the method that is called when the submit button is clicked
+    private class emailVerificationResult {
+        public String statusNbr;
+        public String hygieneResult;
+    }
 
     public void verifyEmail(View view) {
 
@@ -32,9 +38,10 @@ public class MainActivity extends AppCompatActivity {
 
             new CallAPI().execute(urlString);
 
-        // TODO, create the task to call the REST API
+            // TODO, create the task to call the REST API
 
-    }
+        }
+
     private class CallAPI extends AsyncTask<String, String, String> {
 
         @Override
@@ -46,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
             String resultToDisplay = "";
 
             InputStream in = null;
+            emailVerificationResult result = null;
 
             // HTTP Get
             try {
@@ -64,11 +72,58 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
+            // Parse XML
+            XmlPullParserFactory pullParserFactory;
+
+            try {
+                pullParserFactory = XmlPullParserFactory.newInstance();
+                XmlPullParser parser = pullParserFactory.newPullParser();
+
+                parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+                parser.setInput(in, null);
+                result = parseXML(parser);
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Simple logic to determine if the email is dangerous, invalid, or valid
+            if (result != null ) {
+                if( result.hygieneResult.equals("Spam Trap")) {
+                    resultToDisplay = "Dangerous email, please correct";
+                }
+                else if( Integer.parseInt(result.statusNbr) >= 300) {
+                    resultToDisplay = "Invalid email, please re-enter";
+                }
+                else {
+                    resultToDisplay = "Thank you for your submission";
+                }
+            }
+            else {
+                resultToDisplay = "Exception Occured";
+            }
+
             return resultToDisplay;
+
+        }
+        protected void onPostExecute(String result) {
+            Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+
+            intent.putExtra(EXTRA_MESSAGE, result);
+
+            startActivity(intent);
+
         }
 
-        public final static String strikeIronUserName = "muenzuve@gmail.com";
-        public final static String strikeIronPassword = "DDvEEo";
-        public final static String apiURL = "http://ws.strikeiron.com/StrikeIron/EMV6Hygiene/VerifyEmail?";
+    } // end CallAP
+
+
+
+        // This is the method that is called when the submit button is clicked
+
+
+
+
     }
 }
